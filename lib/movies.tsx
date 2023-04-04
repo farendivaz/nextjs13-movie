547fe9a48ec460462da7814e5c87a1c84938d99c
Farendivaz <dandyfariz9@gmail.com>
2023-04-04 14:10:15 +0700

    fix api key

--- lib/movies.tsx
+++ lib/movies.tsx
@@ -1,7 +1,7 @@
 // 946df6f6dd6e733f1ab94bd24eeb052b
 export async function getMovies(type: string) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${type}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${type}?api_key=946df6f6dd6e733f1ab94bd24eeb052b`
   );
   const data = await res.json();
   return data.results;
@@ -9,7 +9,7 @@ export async function getMovies(type: string) {
 
 export async function getMovieDetails(id: number) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${id}?api_key=946df6f6dd6e733f1ab94bd24eeb052b&language=en-US`
   );
   const data = await res.json();
 
@@ -18,7 +18,7 @@ export async function getMovieDetails(id: number) {
 
 export async function getMovieCasts(id: number) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=946df6f6dd6e733f1ab94bd24eeb052b&language=en-US`
   );
 
   const data = await res.json();
@@ -28,7 +28,7 @@ export async function getMovieCasts(id: number) {
 
 export async function getMovieRecom(id: number) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=946df6f6dd6e733f1ab94bd24eeb052b&language=en-US`
   );
 
   const data = await res.json();
@@ -38,7 +38,7 @@ export async function getMovieRecom(id: number) {
 
 export async function getPersonMovies(id: number) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=${process.env.API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/person/${id}/combined_credits?api_key=946df6f6dd6e733f1ab94bd24eeb052b&language=en-US`
   );
 
   const data = await res.json();
@@ -48,7 +48,7 @@ export async function getPersonMovies(id: number) {
 
 export async function getPersonDetails(id: number) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/person/${id}?api_key=946df6f6dd6e733f1ab94bd24eeb052b&language=en-US`
   );
 
   const data = await res.json();
@@ -58,7 +58,7 @@ export async function getPersonDetails(id: number) {
 
 export async function getMovieResults(search: string) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=946df6f6dd6e733f1ab94bd24eeb052b&language=en-US&query=${search}&page=1&include_adult=false`
   );
 
   const data = await res.json();
